00308b389a3f42974f2d071eeafbabd0479c7fa9
ncodena <c.odena.nuria@gmail.com>
2024-01-25 17:03:30 +0100

    adding code

--- controllers/filmsController.js
+++ controllers/filmsController.js
@@ -18,7 +18,6 @@ let films = [
 
 export const getFilms = (req, res) => {
     const searchTerm = req.query.search;
-    console.log(searchTerm)
  
     // If no search term provided, retrieve all films
     if (!searchTerm) {
@@ -30,9 +29,9 @@ export const getFilms = (req, res) => {
     const matchedFilms = films.filter(film => 
         film.name.toLowerCase().includes(searchTerm.toLowerCase()));
 
- 
+    // If the filter gets no results, 404 Not Found status is returned to the client with a message indicating that no records match the criteria.
     if (matchedFilms.length === 0) {
-      return res.status(404).send('Film not found');
+      return res.status(404).send('Films not found');
     }
  
     res.json(matchedFilms);
@@ -42,8 +41,9 @@ export const getFilm = (req, res) => {
     const {id} = req.params;
 
     //ID is parsed from a string to an integer to ensure that it can be compared to the id property of each recipe, which is type number. 
-    const film = films.find(film => film.id === parseInt(id))
-   
+    const film = films.find(film => film.id === parseInt(id));
+
+    //If no film is found, 404 Not Found status is returned to the client with a message indicating that the resource was not found.
     if (!film) {
       return res.status(404).send('Film not found')
     }
